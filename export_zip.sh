9036e3688f7a0531d4184d7d96f2ed71daaa20da
Arthus de Saint Chaffray <arthusdesaintchaffray@gmail.com>
2018-11-15 18:06:47 +0100

    add sortedcontainers to parser

--- export_zip.sh
+++ export_zip.sh
@@ -7,7 +7,7 @@ zip -r -9 -q $LAMBDA_BUILD/completer.zip \
         urllib3 requests idna certifi chardet pytz googlemaps bugsnag
 
 zip -r -9 -q $LAMBDA_BUILD/parser.zip \
-        urllib3 requests idna chardet certifi pdfminer simplejson pandas xlrd bugsnag pytz numpy
+        urllib3 requests idna chardet certifi pdfminer simplejson pandas xlrd bugsnag pytz numpy sortedcontainers
 
 zip -r -9 -q $LAMBDA_BUILD/predictor.zip \
         urllib3 requests idna chardet certifi pytz numpy scipy sklearn xgboost bugsnag
