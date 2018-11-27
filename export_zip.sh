8a5809a7f2c829cdc13faec097bf59e1242c4c1a
Arthus de Saint Chaffray <arthusdesaintchaffray@gmail.com>
2018-11-27 15:09:10 +0100

    add pandas in predictor.zip

--- export_zip.sh
+++ export_zip.sh
@@ -10,12 +10,15 @@ zip -r -9 -q $LAMBDA_BUILD/parser.zip \
         urllib3 requests idna chardet certifi pdfminer simplejson pandas xlrd bugsnag pytz numpy sortedcontainers
 
 zip -r -9 -q $LAMBDA_BUILD/predictor.zip \
-        urllib3 requests idna chardet certifi pytz numpy scipy sklearn xgboost bugsnag
+        urllib3 requests idna chardet certifi pytz numpy scipy sklearn xgboost bugsnag pandas
 
 zip -r -9 -q $LAMBDA_BUILD/placement.zip \
         urllib3 requests idna chardet certifi pytz numpy pandas bugsnag
 
 zip -r -9 -q $LAMBDA_BUILD/soget_processor.zip \
         urllib3 requests idna certifi chardet pytz googlemaps xmltodict bugsnag
+
+zip -r -9 -q $LAMBDA_BUILD/weather.zip \
+        numpy urllib3 requests idna chardet certifi bugsnag
 
 popd
